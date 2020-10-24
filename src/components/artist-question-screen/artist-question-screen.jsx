e94df8e38561760ca98e12385af599f380dd1e79
aalexceenko <xnyxnymrik@bk.ru>
2020-10-24 22:11:41 +0300

    added audio-player with HOC

--- src/components/artist-question-screen/artist-question-screen.jsx
+++ src/components/artist-question-screen/artist-question-screen.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {GameType} from "../../const";
+import artistQuestionProp from "./artist-question.prop";
 
-const ArtistQuestionScreen = ({onAnswer, question}) => {
+
+const ArtistQuestionScreen = (props) => {
+  const {onAnswer, question, renderPlayer} = props;
   const {
     answers,
     song,
@@ -32,10 +34,7 @@ const ArtistQuestionScreen = ({onAnswer, question}) => {
         <h2 className="game__title">Кто исполняет эту песню?</h2>
         <div className="game__track">
           <div className="track">
-            <button className="track__button track__button--play" type="button" />
-            <div className="track__status">
-              <audio src={song.src} />
-            </div>
+            {renderPlayer(song.src, 0)}
           </div>
         </div>
 
@@ -60,19 +59,11 @@ const ArtistQuestionScreen = ({onAnswer, question}) => {
   );
 };
 
+
 ArtistQuestionScreen.propTypes = {
   onAnswer: PropTypes.func.isRequired,
-  question: PropTypes.shape({
-    answers: PropTypes.arrayOf(PropTypes.shape({
-      artist: PropTypes.string.isRequired,
-      picture: PropTypes.string.isRequired,
-    })).isRequired,
-    song: PropTypes.shape({
-      artist: PropTypes.string.isRequired,
-      src: PropTypes.string.isRequired,
-    }).isRequired,
-    type: PropTypes.oneOf([GameType.ARTIST, GameType.GENRE]).isRequired,
-  }).isRequired,
+  question: artistQuestionProp,
+  renderPlayer: PropTypes.func.isRequired,
 };
 
 export default ArtistQuestionScreen;
